5759e25eb498180d33bb1a846f7621b2d227a65f
Sonu Kapoor <sonu@solidsoftwaresolutions.ca>
2016-09-16 10:15:09 -0400

    Removed hardcoded data object

--- app/people.service.ts
+++ app/people.service.ts
@@ -4,12 +4,6 @@ import { Http, Response, Headers} from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 
-const PEOPLE: Person[] = [
-  { id: 1, name: 'Luke Skywalker', height: 177, weight: 70 },
-  { id: 2, name: 'Darth Vader', height: 200, weight: 100 },
-  { id: 3, name: 'Han Solo', height: 185, weight: 85 },
-];
-
 @Injectable()
 export class PeopleService {
 
